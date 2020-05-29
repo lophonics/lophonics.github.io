27d101519d10dcf70dcc24619e5eb2c44fba9fd5
Benedict Steele <benedictsteele@Benedicts-MacBook-Pro-2.local>
2020-05-29 11:05:22 +0100

    adding src

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,36 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    siteUrl: 'http://lophonics.com'
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        typekit: {
+          id: 'ock6pcx'
+        }
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-sass'
+    },
+    {
+      resolve: 'gatsby-plugin-cname'
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-167916788-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import "./styles.scss"
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>LoPhonics</h1>
+    <h2>Post-Production Audio |  Music &amp; Sound</h2>
+    <div class="showreel"><iframe title="showreel" src="https://player.vimeo.com/video/421513161?color=ffffff&title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>
+    <p>Find us on <a href="https://fb.me/lophonics">Facebook</a> and <a href="https://twitter.com/lophonics">Twitter</a> or chat to us <a href="https://m.me/lophonics">here</a>.</p>
+    <p>&copy; 2020</p>
+    </div>
 }
